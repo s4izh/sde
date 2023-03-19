a9f782e9bf2cafa1b1f3e7ec9b192137b092d31c
s4izh <sergiosanz234@gmail.com>
2023-03-19 19:31:02 +0100

    all

--- lua/sergio/plugins/markdown.lua
+++ lua/sergio/plugins/markdown.lua
@@ -1,14 +1,8 @@
 return {
     'dhruvasagar/vim-table-mode',
     'preservim/vim-markdown',
-    "iamcco/markdown-preview.nvim",
-    build = "cd app && npm install",
-    config = function()
-        vim.g.mkdp_filetypes = { "markdown" }
-    end,
-    ft = { "markdown" }
-    -- "vim-pandoc/vim-pandoc",
-    -- "vim-pandoc/vim-pandoc-syntax",
+    "vim-pandoc/vim-pandoc",
+    "vim-pandoc/vim-pandoc-syntax",
     -- 'plasticboy/vim-markdown',
     -- 'lervag/wiki.vim',
 }
