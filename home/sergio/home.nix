19dc9017d81a97c66c127770ae88986dc05873f5
s4izh <sergiosanz234@gmail.com>
2023-03-22 22:50:20 +0100

    first commit

---
+++ home/sergio/home.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, lib, ... }:
+{
+  xdg = {
+    enable = true;
+    userDirs = {
+      enable = true;
+      createDirectories = true;
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    #userName = "s4izh";
+    #userEmail = "sergiosanz234@gmail.com";
+    #defaultBranch = "main";
+  };
+
+  xdg.configFile."git/config".source = ./.config/git/config;
+
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "github" = {
+        hostname = "github";
+        user = "git";
+        identityFile = "/home/sergio/.ssh/github";
+      };
+    };
+  };
+
+
+  home.stateVersion = "22.11";
+}
