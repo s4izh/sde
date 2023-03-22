adafc90ba82677a9cb17b4d25bc59374d937998d
s4izh <sergiosanz234@gmail.com>
2023-03-22 22:50:20 +0100

    first commit

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    #home.manager.url = "github:nix-community/home-manager";
+    #home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, nixpkgs, home-manager, ... }:
+    let 
+      pkgs = import nixpkgs { 
+        #inherit system;
+        config.allowUnfree = true;
+      };
+      lib = nixpkgs.lib;
+    in {
+      nixosConfigurations = {
+        z390 = lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [ 
+            ./configuration.nix
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                extraSpecialArgs = { inherit inputs; };
+                users.sergio.imports = [
+                  ./home/sergio/home.nix
+                ];
+              };
+            }
+          ];
+         };
+      };
+    };
+}
+    
