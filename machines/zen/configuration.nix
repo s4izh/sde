3545a0147de5861024e53aaedd333e9122a31398
s4izh <sergiosanz234@gmail.com>
2023-03-25 23:53:46 +0100

    modules for z390 machine

--- machines/zen/configuration.nix
+++ machines/zen/configuration.nix
@@ -12,15 +12,9 @@
     ];
 
   networking.hostName = "zen";
+  networking.networkmanager.enable = true;
 
   hardware.bluetooth.enable = true;
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-  # Enable networking
-  networking.networkmanager.enable = true;
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
