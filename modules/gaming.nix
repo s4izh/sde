a0344c9b78f75bf8616200a8efac217ee9481c04
s4izh <sergiosanz234@gmail.com>
2023-03-25 13:26:26 +0100

    programas nuevos basicos

--- modules/gaming.nix
+++ modules/gaming.nix
@@ -15,10 +15,8 @@
   #Steam
   programs.steam = {
     enable = true;
-    remotePlay.openFirewall =
-      true; # Open ports in the firewall for Steam Remote Play
-      dedicatedServer.openFirewall =
-      true; # Open ports in the firewall for Source Dedicated Server
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
   };
 
   #G29 wheel
