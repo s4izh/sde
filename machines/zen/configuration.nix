02892cf8d45141fc634ced99d863a2c9fd4bfe97
s4izh <sergiosanz234@gmail.com>
2023-03-26 16:58:57 +0200

    Nvidia Drivers for main workstation

--- machines/zen/configuration.nix
+++ machines/zen/configuration.nix
@@ -8,13 +8,14 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
-      ./battery.nix
+      #./battery.nix
     ];
 
   networking.hostName = "zen";
   networking.networkmanager.enable = true;
 
   hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -22,12 +23,23 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-  services.tlp.enable = true;
+  #services.tlp.enable = true;
 
+  services.tlp = {
+    enable = true;
+    settings = {
+#      SOUND_POWER_SAVE_ON_AC = 1;
+#      SOUND_POWER_SAVE_ON_BAT = 1;
+#      RUNTIME_PM_ON_AC = "auto";
+      PCIE_ASPM_ON_AC = "powersave";
+      PCIE_ASPM_ON_BAT = "powersave";
+    };
+  };
 
   environment.systemPackages = with pkgs; [
     powertop
     acpi
+    brightnessctl
   ];
 
   system.stateVersion = "22.11";
