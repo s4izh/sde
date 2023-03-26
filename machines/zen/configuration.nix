0fdff65bbaf710cf143d0b30f5da32d53fe0afa2
s4izh <sergiosanz234@gmail.com>
2023-03-26 17:05:53 +0200

    amdgpu drivers for zen

--- machines/zen/configuration.nix
+++ machines/zen/configuration.nix
@@ -17,20 +17,18 @@
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
 
+  services.xserver.videoDrivers = [ "amdgpu" ];
+  hardware.opengl.enable = true;
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
-  #services.tlp.enable = true;
-
   services.tlp = {
     enable = true;
     settings = {
-#      SOUND_POWER_SAVE_ON_AC = 1;
-#      SOUND_POWER_SAVE_ON_BAT = 1;
-#      RUNTIME_PM_ON_AC = "auto";
       PCIE_ASPM_ON_AC = "powersave";
       PCIE_ASPM_ON_BAT = "powersave";
     };
