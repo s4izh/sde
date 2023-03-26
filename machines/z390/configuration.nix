0da91396f77919af9060edb40c96c4402ea9b25e
s4izh <sergiosanz234@gmail.com>
2023-03-26 16:58:57 +0200

    Nvidia Drivers for main workstation

--- machines/z390/configuration.nix
+++ machines/z390/configuration.nix
@@ -18,4 +18,15 @@
   boot.loader.systemd-boot.configurationLimit = 5;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
+
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+
+  hardware.opengl.enable = true;
+
+  # Optionally, you may need to select the appropriate driver version for your specific GPU.
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+
+  # nvidia-drm.modeset=1 is required for some wayland compositors, e.g. sway
+  hardware.nvidia.modesetting.enable = true;
 }
