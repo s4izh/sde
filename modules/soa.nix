16f89e9ef453c4ef9c949c5017b5ca34110c7d28
s4izh <sergiosanz234@gmail.com>
2023-03-28 00:34:54 +0200

    soa

---
+++ modules/soa.nix
@@ -0,0 +1,8 @@
+{ pkgs, lib, inputs, ... }:
+{
+  environment.systemPackages = [
+    inputs.bochs-soa.packages.x86_64-linux.bochs
+    inputs.bochs-soa.packages.x86_64-linux.default
+    pkgs.gcc9
+  ];
+}
