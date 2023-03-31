4a075508533cd4aaacbb8c8a8420e65455a714e2
s4izh <sergiosanz234@gmail.com>
2023-03-31 15:42:56 +0200

    deleted soa module (now a project flake)

--- modules/soa.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, lib, inputs, ... }:
-{
-  environment.systemPackages = [
-    inputs.bochs-soa.packages.x86_64-linux.bochs
-    inputs.bochs-soa.packages.x86_64-linux.default
-    pkgs.gcc9
-    pkgs.dev86
-  ];
-}
