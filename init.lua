e79ddee0148dce5e4b644575e0ed4c06d49693af
s4izh <sergiosanz234@gmail.com>
2023-05-20 15:26:52 +0200

    fixed sway highlighting if the folder is softlinked

--- init.lua
+++ init.lua
@@ -8,4 +8,5 @@ vim.opt.signcolumn="no"
 
 vim.cmd([[
   autocmd BufNewFile,BufRead ~/.config/sway/config.d/* set filetype=swayconfig
+  autocmd BufNewFile,BufRead ~/.dotfiles/.config/sway/config.d/* set filetype=swayconfig
 ]])
