a50b28437434b7a44196abd3c441a070e95f77f9
s4izh <sergiosanz234@gmail.com>
2023-06-08 10:30:56 +0200

    no spell bind

--- lua/sergio/keybinds.lua
+++ lua/sergio/keybinds.lua
@@ -95,6 +95,7 @@ vim.keymap.set("n", "<Leader>Ca", ":set colorcolumn=0<cr>")
 vim.keymap.set("n", "<Leader>wsc", ":setlocal spell spelllang=ca<cr>")
 vim.keymap.set("n", "<Leader>wse", ":setlocal spell spelllang=es<cr>")
 vim.keymap.set("n", "<Leader>wsi", ":setlocal spell spelllang=en<cr>")
+vim.keymap.set("n", "<Leader>wS", ":setlocal nospell<cr>")
 vim.keymap.set("n", "<Leader>wT", ":r!~/pkm/src/template %<cr>ggdd")
 -- vim.keymap.set("n", "<Leader>wsn", ":set nospell")
 
