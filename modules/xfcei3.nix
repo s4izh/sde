e2f645dc02f27acbbc94a4fa6706621cbb81240b
s4izh <sergiosanz234@gmail.com>
2023-06-09 15:06:36 +0200

    jetbrains

---
+++ modules/xfcei3.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, lib, ... }:
+{
+  services.xserver = {
+    enable = true;
+    desktopManager = {
+      xterm.enable = false;
+      xfce = {
+        enable = true;
+        noDesktop = true;
+        enableXfwm = false;
+      };
+    };
+    displayManager.defaultSession = "xfce";
+    windowManager.i3.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    dmenu
+    # sxhkd
+    xorg.xwininfo
+  ];
+
+  nixpkgs.overlays = [
+    (final: prev: {
+     dmenu = prev.dmenu.overrideAttrs (old: {
+         src = /home/sergio/.local/src/dmenu ;
+         });
+     })
+  ];
+
+
+  system.stateVersion = "23.05"; # Did you read the comment? yes
+}
