e313b3a5ed5abcea64f20aa910bd4757998b06d7
s4izh <sergiosanz234@gmail.com>
2023-06-10 14:38:19 +0200

    removed picom

--- .local/x_autostart.sh
+++ .local/x_autostart.sh
@@ -8,7 +8,7 @@
 feh --bg-fill $HOME/pix/system-wallpaper
 
 # picom no slow animations settings
-picom --fade-in-step=1 --fade-out-step=1 --fade-delta=0 &
+# picom --fade-in-step=1 --fade-out-step=1 --fade-delta=0 &
 
 # network manager systray
 nm-applet --indicator &
