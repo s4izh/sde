d8562b187afe7aa45111172cf0d7c3e7445472c5
s4izh <sergiosanz234@gmail.com>
2023-06-10 12:07:19 +0200

    all

--- lua/sergio/keybinds.lua
+++ lua/sergio/keybinds.lua
@@ -97,7 +97,7 @@ vim.keymap.set("n", "<Leader>wse", ":setlocal spell spelllang=es<cr>")
 vim.keymap.set("n", "<Leader>wsi", ":setlocal spell spelllang=en<cr>")
 vim.keymap.set("n", "<Leader>wS", ":setlocal nospell<cr>")
 vim.keymap.set("n", "<Leader>wT", ":r!~/pkm/src/template %<cr>ggdd")
--- vim.keymap.set("n", "<Leader>wsn", ":set nospell")
+-- vim.keymap.set("n", "<Leader>wn", ":set nospell")
 
 vim.keymap.set("n", "<Leader>wf", ":set ft=")
 
