bca79b29090de6c10986226bd9cbafed716d29b7
s4izh <sergiosanz234@gmail.com>
2023-06-10 22:27:48 +0200

    ssh config

--- machines/zen/hardware-configuration.nix
+++ machines/zen/hardware-configuration.nix
@@ -31,7 +31,6 @@
     "resolv.conf".text = "nameserver 8.8.8.8\n";
   };
 
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
