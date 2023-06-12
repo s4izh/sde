92ed7e540b702fa790dc32719737e4a366bb75dd
s4izh <sergiosanz234@gmail.com>
2023-06-12 10:37:13 +0200

    nix shell for deployment

---
+++ shell.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> { } }:
+pkgs.mkShell {
+  name = "nixosbuildshell";
+  nativeBuildInputs = with pkgs; [
+    git
+    git-crypt
+    nixFlakes
+  ];
+
+  shellHook = ''
+      echo "make help"
+      PATH=${pkgs.writeShellScriptBin "nix" ''
+        ${pkgs.nixFlakes}/bin/nix --experimental-features "nix-command flakes" "$@"
+      ''}/bin:$PATH
+    '';
+}
