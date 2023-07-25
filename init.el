b3fbc0fd101ba02ebcaf97f920a2338fb734b990
s4izh <sergiosanz234@gmail.com>
2023-07-26 01:02:58 +0200

    popper compile disabled

--- init.el
+++ init.el
@@ -400,9 +400,9 @@
   (setq popper-reference-buffers
         '("\\*Messages\\*"
           "Output\\*$"
-          "\\*Async Shell Command\\*"
+          "\\*Async Shell Command\\*"))
           ;; help-mode))
-          compilation-mode))
+          ;; compilation-mode))
   (popper-mode +1)
   (popper-echo-mode +1))                ; For echo area hints
 
