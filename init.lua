2237d5d7bf2439135f52550b5d45cf398c982418
s4izh <sergiosanz234@gmail.com>
2023-07-30 13:55:43 +0200

    changed tmux-navigator to <M-key>, enabled harpoon

--- init.lua
+++ init.lua
@@ -77,4 +77,13 @@ vim.cmd([[
   let g:slime_paste_file = tempname()
 ]])
 
-require('telescope').load_extension('manix')
+vim.g.tmux_navigator_no_mappings = 1
+
+vim.cmd([[
+let g:tmux_navigator_no_mappings = 1
+noremap <silent> <M-h> :<C-U>TmuxNavigateLeft<cr>
+noremap <silent> <M-j> :<C-U>TmuxNavigateDown<cr>
+noremap <silent> <M-k> :<C-U>TmuxNavigateUp<cr>
+noremap <silent> <M-l> :<C-U>TmuxNavigateRight<cr>
+noremap <silent> <M-\> :<C-U>TmuxNavigatePrevious<cr>
+]])
