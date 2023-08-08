f9370b5da474044b7a38aec95af67c6ac9c021c9
s4izh <sergiosanz234@gmail.com>
2023-08-09 00:31:54 +0200

    zen refactor

--- machines/zen/configuration.nix
+++ machines/zen/configuration.nix
@@ -4,11 +4,6 @@
 
 { config, pkgs, ... }:
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
-
   networking.hostName = "zen";
   networking.networkmanager.enable = true;
 
@@ -47,13 +42,4 @@
   services.logind.extraConfig = ''
     HandlePowerKey=ignore
   '';
-
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.11";
 }
