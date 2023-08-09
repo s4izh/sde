9b6198fffd71594d3655657ca838fc43a109fea4
s4izh <sergiosanz234@gmail.com>
2023-08-09 18:30:12 +0200

    format

--- lua/sergio/plugins/lsp.lua
+++ lua/sergio/plugins/lsp.lua
@@ -5,21 +5,21 @@ local opts = {
     branch = 'v1.x',
     dependencies = {
         -- LSP Support
-        { 'neovim/nvim-lspconfig' }, -- Required
-        { 'williamboman/mason.nvim' }, -- Optional
+        { 'neovim/nvim-lspconfig' },             -- Required
+        { 'williamboman/mason.nvim' },           -- Optional
         { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
         -- Autocompletion
-        { 'hrsh7th/nvim-cmp' }, -- Required
-        { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-        { 'hrsh7th/cmp-buffer' }, -- Optional
-        { 'hrsh7th/cmp-path' }, -- Optional
+        { 'hrsh7th/nvim-cmp' },         -- Required
+        { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+        { 'hrsh7th/cmp-buffer' },       -- Optional
+        { 'hrsh7th/cmp-path' },         -- Optional
         { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-        { 'hrsh7th/cmp-nvim-lua' }, -- Optional
+        { 'hrsh7th/cmp-nvim-lua' },     -- Optional
         { 'aspeddro/cmp-pandoc.nvim' },
 
         -- Snippets
-        { 'L3MON4D3/LuaSnip' }, -- Required
+        { 'L3MON4D3/LuaSnip' },             -- Required
         { 'rafamadriz/friendly-snippets' }, -- Optional
     },
     config = function()
@@ -98,13 +98,13 @@ local opts = {
         })
 
         lsp.setup_nvim_cmp({
-          sources = {
-            -- {name = 'path'},
-            -- {name = 'nvim_lsp'},
-            -- {name = 'buffer', keyword_length = 3},
-            -- {name = 'luasnip', keyword_length = 2},
-            -- {name = 'cmp_pandoc', keyword_length = 2},
-          }
+            sources = {
+                -- {name = 'path'},
+                -- {name = 'nvim_lsp'},
+                -- {name = 'buffer', keyword_length = 3},
+                -- {name = 'luasnip', keyword_length = 2},
+                -- {name = 'cmp_pandoc', keyword_length = 2},
+            }
         })
     end
 }
--- lua/sergio/plugins/markdown.lua
+++ lua/sergio/plugins/markdown.lua
@@ -1,8 +1,8 @@
 return {
     'dhruvasagar/vim-table-mode',
     'preservim/vim-markdown',
-    "vim-pandoc/vim-pandoc",
-    "vim-pandoc/vim-pandoc-syntax",
+    -- "vim-pandoc/vim-pandoc",
+    -- "vim-pandoc/vim-pandoc-syntax",
     -- 'plasticboy/vim-markdown',
     -- 'lervag/wiki.vim',
 }
