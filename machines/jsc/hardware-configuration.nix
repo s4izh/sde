8af8c8afc7b240335e38af730cfd7faf69e15386
s4izh <sergiosanz234@gmail.com>
2023-11-08 19:48:00 +0100

    jsc update

--- machines/jsc/hardware-configuration.nix
+++ machines/jsc/hardware-configuration.nix
@@ -36,5 +36,5 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   # high-resolution display
-  hardware.video.hidpi.enable = lib.mkDefault true;
+  # hardware.video.hidpi.enable = lib.mkDefault true;
 }
