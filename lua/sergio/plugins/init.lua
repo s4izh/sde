99148380c00232f15b4c1d2e5fe503807254ed1c
s4izh <sergiosanz234@gmail.com>
2023-12-02 16:38:41 +0100

    init plugins refactor

--- lua/sergio/plugins/init.lua
+++ lua/sergio/plugins/init.lua
@@ -1,6 +1,8 @@
 return {
 
   "tpope/vim-commentary",
+  "tpope/vim-dispatch",
+  "tpope/vim-surround",
   -- "junegunn/fzf.vim",
   -- "junegunn/fzf",
   "junegunn/goyo.vim",
@@ -9,39 +11,35 @@ return {
   "halkn/ripgrep.vim",
   "github/copilot.vim",
   "christoomey/vim-tmux-navigator",
-  "TimUntersberger/neogit",
+  -- "TimUntersberger/neogit",
   "ThePrimeagen/harpoon",
   "xiyaowong/transparent.nvim",
   -- "ctrlpvim/ctrlp.vim",
   "lewis6991/gitsigns.nvim",
   "jpalardy/vim-slime",
-  'mrcjkb/telescope-manix',
+  -- 'mrcjkb/telescope-manix',
   "editorconfig/editorconfig-vim",
   -- "lukas-reineke/indent-blankline.nvim",
   "norcalli/nvim-colorizer.lua",
-  "tpope/vim-dispatch",
-  "tpope/vim-surround",
+  "kmonad/kmonad-vim"; -- support for kmonad files
   "oncomouse/lushwal.nvim",
 	cmd = { "LushwalCompile" },
 	dependencies = {
 		{ "rktjmp/lush.nvim" },
 		{ "rktjmp/shipwright.nvim" },
 	},
+  -- "chrisbra/vim-zsh",
   -- "mhinz/vim-startify",
   -- "startup-nvim/startup.nvim",
-
   -- "HiPhish/nvim-ts-rainbow2",
   -- 'luochen1990/rainbow'
-
     -- use 'tanvirtin/monokai.nvim'
     -- use 'dylanaraps/wal.vim'
     -- use 'Mofiqul/dracula.nvim'
     -- use 'navarasu/onedark.nvim'
-    -- use
     -- use 'svrana/neosolarized.nvim'
     -- use 'maxmx03/solarized.nvim'
     -- use 'squarefrog/tomorrow-night.vim'
     -- use 'wesgibbs/vim-irblack'
-
 }
 
