acf19c23e111ebeaf5ea004b118a7dc8cf3270e6
s4izh <sergiosanz234@gmail.com>
2023-12-02 14:50:03 +0100

    dwm: autostart config (not used)

---
+++ .local/share/dwm/autostart.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+feh --bg-fill ~/pix/wal/V8OmdKN-3937590169.jpg &
+picom &
+nm-applet --indicator &
+dwmblocks &
+dunst &
+sxhkd &
+unclutter -idle 5 &
+xautolock -time 10 -locker slock &
