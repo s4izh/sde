4e86db3d390a1c6544f5a7409dc4782f920a6acb
s4izh <sergiosanz234@gmail.com>
2023-12-10 12:20:55 +0100

    writting: notes directory

--- lua/sergio/writting.lua
+++ lua/sergio/writting.lua
@@ -2,7 +2,7 @@
 -- vim.opt.conceallevel = 2
 -- vim.opt.concealcursor = 'nc'
 
-vim.g.vimwiki_list = {{path = '~/wiki', syntax = 'markdown', ext = '.md'}}
+vim.g.vimwiki_list = {{path = '~/notes', syntax = 'markdown', ext = '.md'}}
 vim.g.vimwiki_markdown_link_ext= 1
 
 -- vim.opt.spelllang='es'
