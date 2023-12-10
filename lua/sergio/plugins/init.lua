a024e7e736abf95ba078d4509e718f8a4aa56f93
s4izh <sergiosanz234@gmail.com>
2023-12-10 12:42:34 +0100

    refactor

--- lua/sergio/plugins/init.lua
+++ lua/sergio/plugins/init.lua
@@ -1,44 +1,50 @@
 return {
-
   "tpope/vim-commentary",
   "tpope/vim-dispatch",
   "tpope/vim-surround",
-  -- "junegunn/fzf.vim",
-  -- "junegunn/fzf",
   "junegunn/goyo.vim",
   "vim-airline/vim-airline",
-  -- "nvim-lualine/lualine.nvim",
   "halkn/ripgrep.vim",
   "christoomey/vim-tmux-navigator",
-  -- "TimUntersberger/neogit",
-  -- "ThePrimeagen/harpoon",
-  "xiyaowong/transparent.nvim",
-  -- "ctrlpvim/ctrlp.vim",
-  "lewis6991/gitsigns.nvim",
   "jpalardy/vim-slime",
-  -- 'mrcjkb/telescope-manix',
   "editorconfig/editorconfig-vim",
-  -- "lukas-reineke/indent-blankline.nvim",
-  "norcalli/nvim-colorizer.lua",
-  "kmonad/kmonad-vim"; -- support for kmonad files
-  "oncomouse/lushwal.nvim",
-	cmd = { "LushwalCompile" },
-	dependencies = {
-		{ "rktjmp/lush.nvim" },
-		{ "rktjmp/shipwright.nvim" },
-	},
+  "kmonad/kmonad-vim", -- support for kmonad files
+  {
+    "xiyaowong/transparent.nvim",
+    config = function()
+      require("transparent").setup()
+      vim.cmd([[TransparentEnable]])
+    end,
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    requires = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      require("gitsigns").setup()
+    end,
+  },
+  {
+    "norcalli/nvim-colorizer.lua",
+    config = function()
+      vim.opt.termguicolors = true
+      require("colorizer").setup()
+    end,
+  },
+  {
+    "oncomouse/lushwal.nvim",
+    cmd = { "LushwalCompile" },
+    dependencies = {
+      { "rktjmp/lush.nvim" },
+      { "rktjmp/shipwright.nvim" },
+    },
+  }
+  -- "ctrlpvim/ctrlp.vim",
   -- "chrisbra/vim-zsh",
   -- "mhinz/vim-startify",
   -- "startup-nvim/startup.nvim",
   -- "HiPhish/nvim-ts-rainbow2",
   -- 'luochen1990/rainbow'
-    -- use 'tanvirtin/monokai.nvim'
-    -- use 'dylanaraps/wal.vim'
-    -- use 'Mofiqul/dracula.nvim'
-    -- use 'navarasu/onedark.nvim'
-    -- use 'svrana/neosolarized.nvim'
-    -- use 'maxmx03/solarized.nvim'
-    -- use 'squarefrog/tomorrow-night.vim'
-    -- use 'wesgibbs/vim-irblack'
+  -- "lukas-reineke/indent-blankline.nvim",
 }
-
