921b911bc2604255858cccf52c5bb3106f361e39
s4izh <sergiosanz234@gmail.com>
2023-12-10 19:52:05 +0100

    telescope: remapped git files to telescope

--- lua/sergio/plugins/telescope.lua
+++ lua/sergio/plugins/telescope.lua
@@ -3,14 +3,17 @@ return {
     tag = '0.1.1',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
-        defaults = {
-          file_ignore_patterns = { "^./img/", "^img/", "^vendor/" },
-        }
+        -- defaults = {
+        --   file_ignore_patterns = { "^./img/", "^img/", "^vendor/" },
+        -- }
         local builtin = require('telescope.builtin')
-        -- vim.keymap.set('n', '<C-f>', builtin.find_files, {})
         vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+        -- vim.keymap.set('n', '<leader>ff', builtin.find_files(require('telescope.themes').get_dropdown, {}))
+        -- vim.cmd [[
+        --     -- nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_cursor({}))<cr>
+        -- ]]
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-        -- vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+        vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
         vim.keymap.set('n', '<leader>fs', function()
