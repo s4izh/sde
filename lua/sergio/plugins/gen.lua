7fe715188eb4aecb1499569f6bb430400391d8c2
s4izh <sergiosanz234@gmail.com>
2023-12-13 09:26:37 +0100

    wip: gen.nvim

--- lua/sergio/plugins/gen.lua
+++ lua/sergio/plugins/gen.lua
@@ -1,7 +1,22 @@
 return {
-    "David-Kunz/gen.nvim",
-    config = function ()
-      local gen = require("gen")
-      gen.model = 'codellama'
-    end
+  "David-Kunz/gen.nvim",
+  config = function()
+    local gen = require("gen")
+    gen.model = 'codellama'
+    vim.keymap.set({ 'n', 'v' }, '<leader>G', ':Gen<CR>')
+    gen.prompts = {
+      document = {
+        prompt =
+        "Document the code in doxygen format, just write the comments nothing else: ```$text```",
+        replace = true,
+        extract = "```$filetype\n(.-)```"
+      },
+      -- document = {
+      --   prompt =
+      --   "Document the code in doxygen format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
+      --   replace = true,
+      --   extract = "```$filetype\n(.-)```"
+      -- },
+    }
+  end
 }
