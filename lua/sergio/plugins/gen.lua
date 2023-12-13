4f636c290cdf69150d59d7a397054041c3ee9805
s4izh <sergiosanz234@gmail.com>
2023-12-13 15:32:00 +0100

    gen: doxygen prompt

--- lua/sergio/plugins/gen.lua
+++ lua/sergio/plugins/gen.lua
@@ -4,19 +4,24 @@ return {
     local gen = require("gen")
     gen.model = 'codellama'
     vim.keymap.set({ 'n', 'v' }, '<leader>G', ':Gen<CR>')
-    gen.prompts = {
-      document = {
-        prompt =
-        "Document the code in doxygen format, just write the comments nothing else: ```$text```",
+    gen.prompts['doxygen'] = {
+        prompt = "Write the doxygen documentation for this code, just write the doxygen documentation, don't output anything else, I only want the comments that go into my file, don't output anything else, this is the code: ```$text```",
         replace = true,
         extract = "```$filetype\n(.-)```"
-      },
+    }
+    -- gen.prompts = {
+    --   document = {
+    --     prompt =
+    --     "Document the code in doxygen format, just write the comments nothing else: ```$text```",
+    --     replace = true,
+    --     extract = "```$filetype\n(.-)```"
+    --   },
       -- document = {
       --   prompt =
       --   "Document the code in doxygen format ```$filetype\n...\n```:\n```$filetype\n$text\n```",
       --   replace = true,
       --   extract = "```$filetype\n(.-)```"
       -- },
-    }
+    -- }
   end
 }
