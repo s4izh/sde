469af762907f6f84c06202accfa1106a623d3ffd
s4izh <sergiosanz234@gmail.com>
2023-12-13 09:26:37 +0100

    wip: gen.nvim

--- lua/sergio/init.lua
+++ lua/sergio/init.lua
@@ -5,3 +5,10 @@ require("sergio.lazy")
 require("sergio.colors")
 require("sergio.gtd")
 require("sergio.sudo")
+
+-- require('telescope').load_extension('gen_nvim', '~/.config/nvim/lua/sergio/telescope-gen.lua')
+
+-- local telescope_gen = require('telescope-gen')
+-- telescope_gen.setup()
+
+-- vim.keymap.set('<leader>G', telescope_gen.select_gen_nvim_model(), {})
