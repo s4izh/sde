4d8049ced68a48934192c71b7670a3649255073c
s4izh <sergiosanz234@gmail.com>
2024-01-08 14:28:01 +0100

    telescope: <leader>fs to grep string under cursor

--- lua/sergio/plugins/telescope.lua
+++ lua/sergio/plugins/telescope.lua
@@ -16,7 +16,8 @@ return {
         vim.keymap.set('n', '<C-p>', builtin.git_files, {})
         vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
         vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-        vim.keymap.set('n', '<leader>fs', function()
+        vim.keymap.set('n', '<leader>fs', builtin.grep_string, {})
+        vim.keymap.set('n', '<leader>fS', function()
             builtin.grep_string({ search = vim.fn.input("Grep > ")});
         end)
     end
