d57c9237702c5ec3d7cc2ecd6eaec7658e9a6f19
s4izh <sergiosanz234@gmail.com>
2024-03-01 18:51:58 +0100

    nvim: nvim overlay

--- modules/base.nix
+++ modules/base.nix
@@ -26,7 +26,7 @@
   nixpkgs.config.allowUnfree = true;
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    neovim
+    # neovim
     vimv
     wget
     git
