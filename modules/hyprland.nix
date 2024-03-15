700b713cf5daadd3bff9c4f9159cd2b2c5d94928
s4izh <sergiosanz234@gmail.com>
2024-03-15 19:20:55 +0100

    wip: add hyprland and dwl changes

---
+++ modules/hyprland.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, lib, ... }:
+{
+  programs.hyprland = {
+    # Install the packages from nixpkgs
+    enable = true;
+    # Whether to enable XWayland
+    xwayland.enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    wl-clipboard
+    waybar
+    rofi
+  ];
+}
