3c20c6c9efade920c1eb96956f1aa42450f041a8
s4izh <sergiosanz234@gmail.com>
2024-03-15 16:44:03 +0100

    tmux: move tmux out of home.nix

--- home/sergio/home.nix
+++ home/sergio/home.nix
@@ -28,7 +28,7 @@ in {
     configFile."mimeapps.list".source = "${dotfiles}/.config/mimeapps.list";
     configFile."libvirt/libvirt.conf".source =
       "${dotfiles}/.config/libvirt/libvirt.conf";
-    configFile."tmux/tmux.conf".source = "${dotfiles}/.config/tmux/tmux.conf";
+    # configFile."tmux/tmux.conf".source = "${dotfiles}/.config/tmux/tmux.conf";
     configFile."zathura/zathurarc".source = "${dotfiles}/.config/zathura/zathurarc";
     # configFile."user-dirs.dirs".source = "${dotfiles}/.config/user-dirs.dirs";
     # configFile."user-dirs.locale".source = "${dotfiles}/.config/user-dirs.locale";
