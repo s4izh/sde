9017f5b5ef27cc70024fe398d92450e49e19a6cd
s4izh <sergiosanz234@gmail.com>
2024-03-15 16:32:07 +0100

    pkgs: moved dev packages to dev.nix

--- modules/dev.nix
+++ modules/dev.nix
@@ -1,11 +1,76 @@
-{ config, pkgs, lib, ... }:
 {
-  environment.systemPackages = with pkgs; [
+  config,
+  pkgs,
+  lib,
+  ...
+}: let
+  rustpkgs = with pkgs; [
+    rustup
+    cargo
+    cargo-watch
+    cargo-make
+    rust-analyzer
+    rustfmt
+    rustc
+  ];
+  latexpkgs = with pkgs; [
+    texlive.combined.scheme-full
+    texlab
+    ltex-ls
+  ];
+  nixpkgs = with pkgs; [
     nixd
     alejandra
-    clang
-    lua-language-server
-    texlab
-    ltex-ls # languagetool
   ];
+  luapkgs = with pkgs; [
+    lua-language-server
+  ];
+  cpkgs = with pkgs; [
+    gcc
+    cmake
+    clang
+    clang-tools
+    gdb
+  ];
+  erlangpkgs = with pkgs; [
+    rebar3
+    erlang
+    erlang-ls
+  ];
+  linuxpkgs = with pkgs; [
+    man-pages
+    mandoc
+    man-db
+  ];
+  shpkgs = with pkgs; [
+    nodePackages_latest.bash-language-server
+    shellcheck
+    shfmt
+  ];
+  gopkgs = with pkgs; [
+    go
+    gopls
+  ];
+  devpkgs = with pkgs; [
+    git
+    tmux
+    fzf
+    gnumake
+    direnv
+    jq
+    pstree
+    cloc
+  ];
+in {
+  environment.systemPackages =
+    devpkgs
+    ++ rustpkgs
+    ++ latexpkgs
+    ++ nixpkgs
+    ++ luapkgs
+    ++ cpkgs
+    ++ erlangpkgs
+    ++ linuxpkgs
+    ++ shpkgs
+    ++ gopkgs;
 }
