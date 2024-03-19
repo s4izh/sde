d2d7d24ef2675e757dcf4fc4b6b344add0964ecb
s4izh <sergiosanz234@gmail.com>
2024-03-19 21:00:25 +0100

    add river module

---
+++ modules/river.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, lib, ... }:
+{
+  # services.xserver = {
+  #   enable = true;
+  #   windowManager.stumpwm.enable = true;
+  #   displayManager.lightdm.enable = true;
+  # };
+
+  programs.river = {
+    enable = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    yambar
+  ];
+
+}
