1df003a29a261a6dfadfed16255eb0704d053adb
s4izh <sergiosanz234@gmail.com>
2024-03-31 01:30:21 +0100

    more settings

--- init.lua
+++ init.lua
@@ -1,3 +1,35 @@
 vim.g.mapleader = " "
 
 require("sergio")
+
+-- highlight when yanking text
+local yank_group = vim.api.nvim_create_augroup('HighlightYank', {})
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = yank_group,
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = 'IncSearch',
+      timeout = 40,
+    })
+  end,
+})
+
+-- status bar
+-- this is from Gavin Freeborn
+vim.cmd([[
+function! Statusline_expr()
+	let mod  = "%{&modified ? '[+] ' : !&modifiable ? '[x] ' : ''}"
+	let ft   = "%{len(&filetype) ? '['.&filetype.'] ' : ''}"
+	let fug  = "%3*%{exists('g:loaded_fugitive') ? fugitive#statusline() : ''}"
+	let job  = "%2*%{exists('g:job') ? '[Job Running!]' : ''}%*"
+	let zoom = "%3*%{exists('t:maximize_hidden_save') ? '[Z]' : ''}%*"
+	let sep  = ' %= '
+	let pos  = ' %-14.(%l,%c%V%) '
+	let pct  = ' %P'
+
+	return '%<%f %<'.mod.fug.job.zoom.sep.pos.pct
+endfunction
+let &statusline = Statusline_expr()
+set laststatus=2 "show statusbar
+]])
