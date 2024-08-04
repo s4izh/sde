447487dd94c68c9e39a1ea7b13e63a4cbb20cc02
s4izh <sergiosanz234@gmail.com>
2024-08-04 17:29:28 +0200

    themes: adjustments

--- lua/sergio/colors.lua
+++ lua/sergio/colors.lua
@@ -1,4 +1,4 @@
-vim.opt.termguicolors = false
+vim.opt.termguicolors = true
 
 function ColorMyPencils(color)
   color = color or "rose-pine"
@@ -11,7 +11,7 @@ function ColorMyPencils(color)
   vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
   -- vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none" })
 
-  -- vim.api.nvim_set_hl(0, "LineNr", { bg = "none", ctermbg = "none" })
+  vim.api.nvim_set_hl(0, "LineNr", { bg = "none", ctermbg = "none", fg="grey" })
 
   vim.cmd [[
       highlight LineNr ctermbg=none guibg=none
@@ -22,14 +22,16 @@ function ColorMyPencils(color)
   vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none", fg = "grey" })  -- Inactive status line color
 
   -- Set the normal text color for inactive windows
-  vim.api.nvim_set_hl(0, "NormalNC", { bg = "none", fg = "grey" })  -- Change 'grey' to your desired color
+  -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none", fg = "grey" })  -- Change 'grey' to your desired color
+  vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })  -- Change 'grey' to your desired color
+
+  vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none", fg = "grey" })  -- Use 'grey' or your preferred color
 end
 
 vim.opt.background = "dark"
-
 
 -- require("rose-pine")
 
 vim.g.gruvbox_contrast_dark = 'hard' -- Set desired contrast here
 
-ColorMyPencils("noctu")
+ColorMyPencils("gruber-darker")
