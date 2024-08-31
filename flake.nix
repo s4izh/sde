29603512e13dc016edbe17167491b68eebf91ab4
s4izh <sergiosanz234@gmail.com>
2024-08-31 10:59:20 +0200

    format: formatted all code with nixfmt-rfc-style

--- flake.nix
+++ flake.nix
@@ -7,90 +7,93 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    minegrub-world-sel-theme =  {
+    minegrub-world-sel-theme = {
       url = "github:Lxtharia/minegrub-world-sel-theme";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    home-manager,
-    ...
-  }: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
-    };
-    lib = nixpkgs.lib;
-  in {
-    nixosConfigurations = let
-      specialArgs = {inherit inputs;};
-      # extraSpecialArgs = { inherit inputs; };
-      mkHostConfig = {
-        host,
-        arch,
-      }: {
-        name = host;
-        value = lib.nixosSystem {
-          system = arch;
-          specialArgs = {inherit inputs;};
-          modules = [
-            ./hosts/${host}
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      home-manager,
+      ...
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+      lib = nixpkgs.lib;
+    in
+    {
+      nixosConfigurations =
+        let
+          # extraSpecialArgs = { inherit inputs; };
+          mkHostConfig =
+            { host, arch }:
             {
-              nix.registry.nixpkgs.flake = inputs.nixpkgs; # nix shell to use system flake
+              name = host;
+              value = lib.nixosSystem {
+                system = arch;
+                specialArgs = {
+                  inherit inputs;
+                };
+                modules = [
+                  ./hosts/${host}
+                  {
+                    nix.registry.nixpkgs.flake = inputs.nixpkgs; # nix shell to use system flake
+                  }
+                ];
+              };
+            };
+          hosts = [
+            {
+              host = "z390";
+              arch = "x86_64-linux";
             }
-            inputs.minegrub-world-sel-theme.nixosModules.default
+            {
+              host = "rx";
+              arch = "x86_64-linux";
+            }
+            {
+              host = "zen";
+              arch = "x86_64-linux";
+            }
+            {
+              host = "jsc";
+              arch = "x86_64-linux";
+            }
+            {
+              host = "vm";
+              arch = "x86_64-linux";
+            }
+          ];
+          autoMachineConfigs = map mkHostConfig hosts;
+
+          machineConfigs = autoMachineConfigs ++ [ ];
+        in
+        builtins.listToAttrs machineConfigs;
+
+      homeManagerConfigurations = {
+        sergio = home-manager.lib.homeManagerConfiguration {
+          # pkgs = nixpkgs.legacyPackages.${system};
+          inherit pkgs;
+          modules = [
+            ./home/sergio/home.nix
+            {
+              home = {
+                username = "sergio";
+                homeDirectory = "/home/sergio";
+                stateVersion = "23.11";
+
+              };
+            }
           ];
         };
       };
-      hosts = [
-        {
-          host = "z390";
-          arch = "x86_64-linux";
-        }
-        {
-          host = "rx";
-          arch = "x86_64-linux";
-        }
-        {
-          host = "zen";
-          arch = "x86_64-linux";
-        }
-        {
-          host = "jsc";
-          arch = "x86_64-linux";
-        }
-        {
-          host = "vm";
-          arch = "x86_64-linux";
-        }
-      ];
-      autoMachineConfigs = map mkHostConfig hosts;
-
-      machineConfigs = autoMachineConfigs ++ [];
-    in
-      builtins.listToAttrs machineConfigs;
-
-    homeManagerConfigurations = {
-      sergio = home-manager.lib.homeManagerConfiguration {
-        # pkgs = nixpkgs.legacyPackages.${system};
-        inherit pkgs;
-        modules = [
-          ./home/sergio/home.nix
-          {
-            home = {
-              username = "sergio";
-              homeDirectory = "/home/sergio";
-              stateVersion = "23.11";
-
-            };
-          }
-        ];
-      };
+      packages.x86_64-linux.default = import ./shell.nix { inherit pkgs; };
     };
-    packages.x86_64-linux.default = import ./shell.nix {inherit pkgs;};
-  };
 }
