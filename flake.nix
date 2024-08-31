b80568602a3780506192d99c496bcf68f0a2b4df
s4izh <sergiosanz234@gmail.com>
2024-08-31 10:42:58 +0200

    flake: make minegrub theme follow nixpkgs

--- flake.nix
+++ flake.nix
@@ -7,7 +7,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    minegrub-world-sel-theme.url = "github:Lxtharia/minegrub-world-sel-theme";
+    minegrub-world-sel-theme =  {
+      url = "github:Lxtharia/minegrub-world-sel-theme";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs @ {
