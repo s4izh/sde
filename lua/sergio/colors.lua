3230a75eebe40bc39a6311b6e2352adc77e7a4f7
s4izh <sergiosanz234@gmail.com>
2024-09-01 17:24:59 +0200

    colors: cleanup

--- lua/sergio/colors.lua
+++ lua/sergio/colors.lua
@@ -1,4 +1,7 @@
 vim.opt.termguicolors = true
+vim.opt.background = "dark"
+
+vim.g.gruvbox_contrast_dark = 'hard'
 
 function ColorMyPencils(color)
   color = color or "rose-pine"
@@ -6,18 +9,12 @@ function ColorMyPencils(color)
   vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
   vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-
   vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "white", bg = "none" })
-  vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
-
   vim.api.nvim_set_hl(0, "LineNr", { bg = "none", ctermbg = "none", fg="grey" })
-  -- vim.cmd [[
-  --     highlight LineNr ctermbg=none guibg=none
-  -- ]]
 
-  -- vim.api.nvim_set_hl(0, "StatusLine", { bg = "black" })  -- Transparent status line
-  vim.api.nvim_set_hl(0, "StatusLine", { fg = "white" })  -- Transparent status line
-  vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none", fg = "grey" })  -- Inactive status line color
+  -- vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
+  -- vim.api.nvim_set_hl(0, "StatusLine", { fg = "white" })  -- Transparent status line
+  -- vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "none", fg = "grey" })  -- Inactive status line color
 
   -- Set the normal text color for inactive windows
   -- vim.api.nvim_set_hl(0, "NormalNC", { bg = "none", fg = "grey" })  -- Change 'grey' to your desired color
@@ -26,13 +23,7 @@ function ColorMyPencils(color)
   vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none", fg = "grey" })  -- Use 'grey' or your preferred color
 end
 
-vim.opt.background = "dark"
-
--- require("rose-pine")
-
-vim.g.gruvbox_contrast_dark = 'hard' -- Set desired contrast here
-
-ColorMyPencils("gruber-darker")
+ColorMyPencils("ziggy")
 
 vim.api.nvim_create_user_command("Theme", function(opts)
   ColorMyPencils(opts.args)
