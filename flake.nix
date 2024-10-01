0fc5bd30fb311fe859ea8b7d1a54d35628379cdb
s4izh <sergiosanz234@gmail.com>
2024-10-01 16:34:57 +0200

    ref to nixpkgs branch, and add baobab

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Flake for my multisystem NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
