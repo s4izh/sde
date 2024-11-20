312a8ba5d053140f6da9225bebd0e8bb6b84b9f3
s4izh <sergiosanz234@gmail.com>
2024-11-20 18:19:59 +0100

    nvim: format

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,14 +6,14 @@ require("sergio")
 -- highlight when yanking text
 local yank_group = vim.api.nvim_create_augroup('HighlightYank', {})
 vim.api.nvim_create_autocmd('TextYankPost', {
-  group = yank_group,
-  pattern = '*',
-  callback = function()
-    vim.highlight.on_yank({
-      higroup = 'IncSearch',
-      timeout = 40,
-    })
-  end,
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
 })
 
 vim.api.nvim_set_option("grepprg", "rg --vimgrep --smart-case")
