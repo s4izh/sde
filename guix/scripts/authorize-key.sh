8b5da7bb9476e52bc673a16187c54e1d12a79410
s4izh <sergiosanz234@gmail.com>
2024-11-26 19:07:38 +0100

    guix: move scripts to Makefile

--- guix/scripts/authorize-key.sh
+++
@@ -1,1 +0,0 @@
-sudo guix archive --authorize < signing-key.pub
--- guix/scripts/reconfigure-with-substitutes.sh
+++
@@ -1,1 +0,0 @@
-sudo guix system reconfigure config.scm --substitute-urls='https://ci.guix.gnu.org https://bordeaux.guix.gnu.org https://substitutes.nonguix.org'
