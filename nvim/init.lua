1d1e55c96f7276269edf2abdf259370e50504b07
s4izh <sergiosanz234@gmail.com>
2025-01-03 15:32:38 +0100

    feat(nvim): show hidden files on :grep

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,17 +6,17 @@ require("sergio")
 -- highlight when yanking text
 local yank_group = vim.api.nvim_create_augroup('HighlightYank', {})
 vim.api.nvim_create_autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
+  group = yank_group,
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = 'IncSearch',
+      timeout = 40,
+    })
+  end,
 })
 
-vim.api.nvim_set_option("grepprg", "rg --vimgrep --smart-case")
+vim.api.nvim_set_option("grepprg", "rg --vimgrep --smart-case --hidden")
 vim.api.nvim_set_option("grepformat", "%f:%l:%c:%m,%f:%l:%m")
 
 vim.g.git_base = "HEAD"
