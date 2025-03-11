3ce1a118104083a7f40ae5555c562a011772ff11
s4izh <sergiosanz234@gmail.com>
2025-03-11 12:29:33 +0100

    feat(nixos): add nh

--- nixos/modules/base.nix
+++ nixos/modules/base.nix
@@ -34,6 +34,13 @@
     };
   };
 
+  programs.nh = {
+    enable = true;
+    # clean.enable = true;
+    # clean.extraArgs = "--keep-since 4d --keep 5";
+    # flake = "/";
+  };
+
   nixpkgs.flake.setNixPath = true;
   nixpkgs.flake.setFlakeRegistry = true;
 
