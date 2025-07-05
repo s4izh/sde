7c5505a21e0cb2903c029011fe97a840ab59af51
s4izh <sergiosanz234@gmail.com>
2025-07-05 15:57:47 +0200

    feat(emacs): obsidian support

--- emacs/post-init.el
+++ emacs/post-init.el
@@ -424,3 +424,30 @@
              (result-str (prin1-to-string result)))  ;; Convert result to string
         (kill-new result-str)  ;; Copy result to the clipboard
         (message "Result copied to clipboard: %s" result-str)))))  ;; Display confirmation message
+
+(use-package obsidian
+  :ensure t
+  :config
+  (global-obsidian-mode t)
+  (obsidian-backlinks-mode t)
+  :custom
+  ;; location of obsidian vault
+  (obsidian-directory "~/notes")
+  ;; Default location for new notes from `obsidian-capture'
+  (obsidian-inbox-directory "inbox")
+  ;; Useful if you're going to be using wiki links
+  (markdown-enable-wiki-links nil)
+
+  ;; These bindings are only suggestions; it's okay to use other bindings
+  :bind (:map obsidian-mode-map
+              ;; Create note
+              ("C-c C-n" . obsidian-capture)
+              ;; If you prefer you can use `obsidian-insert-wikilink'
+              ("C-c C-l" . obsidian-insert-link)
+              ;; Open file pointed to by link at point
+              ("C-c C-o" . obsidian-follow-link-at-point)
+              ;; Open a different note from vault
+              ("C-c C-p" . obsidian-jump)
+              ;; Follow a backlink for the current file
+              ("C-c C-b" . obsidian-backlink-jump)))
+
