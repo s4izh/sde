cac68e7380c82760a73162e02120e5152bf46a98
s4izh <sergiosanz234@gmail.com>
2025-07-31 09:33:29 +0200

    feat(nvim): disable treesitter on cmake files

--- nvim/lua/sergio/plugins/treesitter.lua
+++ nvim/lua/sergio/plugins/treesitter.lua
@@ -8,6 +8,7 @@ treesitter.setup({
     highlight = {
         enable = true,
         additional_vim_regex_highlighting = true,
+        disable = { "cmake" },
     },
     indent = {
         enable = true,
