301b85ca56c3cf68a0b0213a2474acdf57257eb8
s4izh <sergiosanz234@gmail.com>
2025-10-18 14:24:47 +0200

    feat(nvim): QoL changes

--- nvim/lua/sergio/lazy/nvim-tree.lua
+++ nvim/lua/sergio/lazy/nvim-tree.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-tree/nvim-tree.lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
   config = function()
     vim.g.nvim_tree_show_icons = {
       git = 0,
